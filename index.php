7e592b9ef9d3da70d25465a7537f2435e4de7d88
deadshotgo <kovalenkoole63@gmail.com>
2021-12-27 13:11:43 +0200

    головна стороніка: робітники додані і відсортировані

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
-<?php include 'admin/controller/Services/all.php'?>
-<?php include 'admin/controller/Text/index.php'?>
+<?php include 'admin/controller/Services/all.php' ?>
+<?php include 'admin/controller/Text/index.php' ?>
+<?php include 'admin/controller/Employee/all.php' ?>
 <!DOCTYPE html>
 <html lang="ru">
 
@@ -13,170 +14,184 @@
 </head>
 
 <body>
-    <header class="header bg">
-        <?php
-            include('menu.php')
-        ?>
-        <div class="container">
-            <div class="header-text-block">
-                <div class="main-title-block">
-                    <div class="logotip-text">
-                        <h3>Rio Barbershop</h3>
-                        <img src="img/icons8-lion-40.png" alt="">
-                    </div>
-                    <h1>Ми допоможемо вам отримати свій новий стиль</h1>
-                    <a href="" class="btn-ser">Записатися на прийом</a>
+<header class="header bg">
+    <?php
+    include('menu.php')
+    ?>
+    <div class="container">
+        <div class="header-text-block">
+            <div class="main-title-block">
+                <div class="logotip-text">
+                    <h3>Rio Barbershop</h3>
+                    <img src="img/icons8-lion-40.png" alt="">
                 </div>
+                <h1>Ми допоможемо вам отримати свій новий стиль</h1>
+                <a href="" class="btn-ser">Записатися на прийом</a>
             </div>
         </div>
-    </header>
-    <section class="about-us">
-        <div class="container">
-            <?php foreach ($user_data as $row) {?>
+    </div>
+</header>
+<section class="about-us">
+    <div class="container">
+        <?php foreach ($user_data as $row) { ?>
             <div class="information-block">
                 <h2><?= $row['title'] ?></h2>
                 <h3><?= $row['pid_title'] ?></h3>
                 <h4><?= $row['text'] ?></h4>
-                    <ul>
-                        <li><?= $row['num1'] ?></li>
-                        <li><?= $row['num2'] ?></li>
-                        <li><?= $row['num3'] ?> </li>
-                    </ul>
+                <ul>
+                    <li><?= $row['num1'] ?></li>
+                    <li><?= $row['num2'] ?></li>
+                    <li><?= $row['num3'] ?> </li>
+                </ul>
             </div>
-            <?php }?>
-        </div>
-    </section>
-    <section class="services light-bg">
-        <div class="container">
-            <div class="services-price">
- <?php foreach ($lim as $col) {?>
+        <?php } ?>
+    </div>
+</section>
+<section class="services light-bg">
+    <div class="container">
+        <div class="services-price">
+            <?php foreach ($lim as $col) { ?>
                 <div class="price-item">
                     <div class="ser-info">
-                        <p> <?php echo $col['title']?> <span>~<?php echo $col['time']?> хв</span></p>
+                        <p> <?php echo $col['title'] ?> <span>~<?php echo $col['time'] ?> хв</span></p>
                     </div>
                     <div class="price">
-                        <p>від <?php echo $col['price']?>грн</p>
+                        <p>від <?php echo $col['price'] ?>грн</p>
                     </div>
                 </div>
-<?php }?>
+            <?php } ?>
 
-            </div>
-            <a href="services.php" class="btn-ser">Оглянути всі послуги</a>
         </div>
-    </section>
-    <section class="our-room">
-        <div class="container">
-            <h2>Ми намагаємося щоб у нас завжди було комфортно та атмосферно</h2>
-            <div class="gallery">
-                <div class="gallery-item">
-                    <img src="img/galleryRoom/roomBig1.jpg" alt="">
-                </div>
-                <div class="gallery-item">
-                    <img src="img/galleryRoom/room1.jpg" alt="">
-                </div>
-                <div class="gallery-item">
-                    <img src="img/galleryRoom/roomBig2.jpg" alt="">
-                </div>
+        <a href="services.php" class="btn-ser">Оглянути всі послуги</a>
+    </div>
+</section>
+<section class="our-room">
+    <div class="container">
+        <h2>Ми намагаємося щоб у нас завжди було комфортно та атмосферно</h2>
+        <div class="gallery">
+            <div class="gallery-item">
+                <img src="img/galleryRoom/roomBig1.jpg" alt="">
             </div>
-            <div class="gallery-second">
-                <div class="gallery-item">
-                    <img src="img/galleryRoom/room2.jpg" alt="">
-                </div>
-                <div class="gallery-item">
-                    <img src="img/galleryRoom/photo_2021-12-14_21-14-31.jpg" alt="">
-                </div>
-                <div class="gallery-item">
-                    <img src="img/galleryRoom/room3.jpg" alt="">
-                </div>
+            <div class="gallery-item">
+                <img src="img/galleryRoom/room1.jpg" alt="">
+            </div>
+            <div class="gallery-item">
+                <img src="img/galleryRoom/roomBig2.jpg" alt="">
             </div>
         </div>
-    </section>
-    <section class="our-masters">
-        <h2>Наші майстри</h2>
-        <div class="master">
-            <div class="info-master">
-                <div class="master-name-hover">
-                    <div class="master-info-text-block">
-                        <img src="img/sertificate.jpg" alt="">
-                    </div>
-                </div>
-                <div class="certificate">
-                    <h3>Сертифікат</h3>
-                </div>
+        <div class="gallery-second">
+            <div class="gallery-item">
+                <img src="img/galleryRoom/room2.jpg" alt="">
             </div>
-            <div class="info-master">
-                <div class="master-name-hover">
-                    <div class="master-info-text-block">
-                        <h3>Ріо</h3>
-                        <p>Барбер з 14 літнім стажем</p>
-                        <p>Корекція бороди ,чистка обличчя та догляд за обличчям ,а також корекція брів ниткою ,фарбування та лікування волосся за допомогою Нанопластики ,Кератину та Ботоксу для волосся.</p>
-                    </div>
-                </div>
-                <img src="img/rio-master.jpg" alt="">
+            <div class="gallery-item">
+                <img src="img/galleryRoom/photo_2021-12-14_21-14-31.jpg" alt="">
+            </div>
+            <div class="gallery-item">
+                <img src="img/galleryRoom/room3.jpg" alt="">
             </div>
         </div>
-        <div class="master">
-            <div class="info-master">
-                <div class="master-name-hover">
-                    <div class="master-info-text-block">
-                        <h3>Ірина</h3>
-                        <p>Я барбер, стаж роботи рік</p>
-                        <p>Закончила курсы барбера в «Rival». Все виды стрижек. Коррекция бороды. Чистка и уход за лицом </p>
+    </div>
+</section>
+<section class="our-masters">
+    <h2>Наші майстри</h2>
+
+
+    <?php $i = 1;
+    foreach ($user_data1 as $row) {
+          if ($i % 2 == 0) { ?>
+            <div class="master">
+                <div class="info-master">
+                    <div class="master-name-hover">
+                        <div class="master-info-text-block">
+                            <img src="/admin/controller/<?= $row['certificate'] ?>" alt="">
+                        </div>
+                    </div>
+                    <div class="certificate">
+                        <h3>Сертифікат</h3>
                     </div>
                 </div>
-                <img src="img/iraMaster.jpg" alt="">
+                <div class="info-master">
+                    <div class="master-name-hover">
+                        <div class="master-info-text-block">
+                            <h3><?= $row['name']  ?></h3>
+                            <p><?= $row['experience'] ?></p>
+                            <p><?= $row['text'] ?></p>
+                        </div>
+                    </div>
+
+                    <img src="/admin/controller/<?= $row['avatar'] ?>" alt="">
+
+                </div>
             </div>
-            <div class="info-master">
-                <div class="master-name-hover">
-                    <div class="master-info-text-block">
-                        <img src="img/sertificate.jpg" alt="">
+        <?php } elseif ($i !== 0) { ?>
+            <div class="master">
+                <div class="info-master">
+                    <div class="master-name-hover">
+                        <div class="master-info-text-block">
+                            <h3><?= $row['name'] ?></h3>
+                            <p><?= $row['experience'] ?></p>
+                            <p><?= $row['text'] ?></p>
+                        </div>
+                    </div>
+                    <img src="/admin/controller/<?= $row['avatar'] ?>" alt="">
+                </div>
+                <div class="info-master">
+                    <div class="master-name-hover">
+                        <div class="master-info-text-block">
+                            <img src="/admin/controller/<?= $row['certificate'] ?>" alt="">
+                        </div>
+                    </div>
+                    <div class="certificate">
+                        <h3>Сертифікат</h3>
                     </div>
                 </div>
-                <div class="certificate">
-                    <h3>Сертифікат</h3>
+            </div>
+        <?php }
+        $i++;
+         }?>
+
+
+</section>
+
+<section class="gallery-block light-bg">
+    <div class="container">
+        <h2>Наші роботи</h2>
+        <div class="slider-wrapper">
+            <div class="slider-gallery">
+                <div class="slide-item active">
+                    <img src="img/gallery/1.jpg" alt="">
+                </div>
+                <div class="slide-item">
+                    <img src="img/gallery/2.jpg" alt="">
+                </div>
+                <div class="slide-item">
+                    <img src="img/gallery/3.jpg" alt="">
+                </div>
+            </div>
+            <div class="slider-gallery">
+                <div class="slide-item-second active">
+                    <img src="img/gallery/4.jpg" alt="">
+                </div>
+                <div class="slide-item-second">
+                    <img src="img/gallery/5.jpg" alt="">
+                </div>
+                <div class="slide-item-second">
+                    <img src="img/gallery/6.jpg" alt="">
                 </div>
             </div>
         </div>
-    </section>
-    <section class="gallery-block light-bg">
-        <div class="container">
-            <h2>Наші роботи</h2>
-            <div class="slider-wrapper">
-                <div class="slider-gallery">
-                    <div class="slide-item active">
-                        <img src="img/gallery/1.jpg" alt="">
-                    </div>
-                    <div class="slide-item">
-                        <img src="img/gallery/2.jpg" alt="">
-                    </div>
-                    <div class="slide-item">
-                        <img src="img/gallery/3.jpg" alt="">
-                    </div>
-                </div>
-                <div class="slider-gallery">
-                    <div class="slide-item-second active">
-                        <img src="img/gallery/4.jpg" alt="">
-                    </div>
-                    <div class="slide-item-second">
-                        <img src="img/gallery/5.jpg" alt="">
-                    </div>
-                    <div class="slide-item-second">
-                        <img src="img/gallery/6.jpg" alt="">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <section class="map">
-    	
-    </section>
-    <?php
-        include('footer.php')
-    ?>
-    <script src="https://kit.fontawesome.com/8c3e319e45.js" crossorigin="anonymous"></script>
-    <script src="js/menu.js"></script>
-    <script src="js/main.js"></script>
-    <script src="js/scroll.js"></script>
+    </div>
+</section>
+<section class="map">
+
+</section>
+<?php
+include('footer.php')
+?>
+<script src="https://kit.fontawesome.com/8c3e319e45.js" crossorigin="anonymous"></script>
+<script src="js/menu.js"></script>
+<script src="js/main.js"></script>
+<script src="js/scroll.js"></script>
 </body>
 
 </html>
