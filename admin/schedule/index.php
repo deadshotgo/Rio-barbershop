6b356de92ee7e8093e1c43ac64d75a2cd9ef14bb
NiK121417 <66626083+NiK121417@users.noreply.github.com>
2021-12-27 17:29:37 +0200

    вывод записи

--- admin/schedule/index.php
+++ admin/schedule/index.php
@@ -7,7 +7,7 @@ include '../controller/Text/index.php';
 <div class="col-md-6">
 
     <div class="card card-primary">
-        <iframe class="schedule-menu" src="https://torrow.net/app/tabs/tab-search/service;id=aae6703ed4122efa7c5b7dca275a45d1c" frameborder="0"></iframe>
+        <iframe class="schedule-menu" src="https://web.torrow.net/app/tabs/tab-personal/order-list;type=UserOrders" frameborder="0"></iframe>
     </div>
     <!-- /.card -->
 </div>
