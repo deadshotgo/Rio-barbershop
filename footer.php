2b82ae8ed1be951385d2d368974857872242b12f
deadshotgo <kovalenkoole63@gmail.com>
2021-12-30 16:56:30 +0200

    правки 3.1

--- footer.php
+++ footer.php
@@ -2,6 +2,7 @@
 <footer class="footer">
 	<div class="container">
 		<div class="footer-block">
+            <?php foreach ($user_data3 as $row ) { ?>
 			<div class="self-information"><a  href="tel:<?= $row['phone']?>">
 				<i class="fas fa-phone-volume"></i>
                 <?php foreach ($user_data3 as $row ) { ?>
